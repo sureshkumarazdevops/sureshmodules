5cc0ae21c6e85cde3dcef12ce20ae5f2fc451b8e
sureshkumarazdevops <108863097+sureshkumarazdevops@users.noreply.github.com>
2022-08-18 13:02:42 +0530

    Create postgres_flexi_server.tf

---
+++ postgres_flexi_server.tf
@@ -0,0 +1,11 @@
+resource "azurerm_postgresql_flexible_server" "postgreserver" {
+  name                   = var.postgresever_name
+  resource_group_name    = azurerm_resource_group.postgres.name
+  location               = azurerm_resource_group.postgres.location
+  version                = "14"
+  administrator_login    = var.admin_name
+  administrator_password = var.admin_password
+  zone                   = "1"
+  storage_mb = 32768
+  sku_name   = "GP_Standard_D4s_v3"
+}
