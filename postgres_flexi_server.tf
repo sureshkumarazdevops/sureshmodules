0359bdc690f5bcbf68a640b45c6358b71852c486
sureshkumarazdevops <108863097+sureshkumarazdevops@users.noreply.github.com>
2022-08-18 15:05:24 +0530

    Update postgres_flexi_server.tf

--- postgres_flexi_server.tf
+++ postgres_flexi_server.tf
@@ -1,5 +1,6 @@
 module "postgress" {
 source = "https://github.com/sureshkumarazdevops/sureshmodules"
+}
 resource "azurerm_postgresql_flexible_server" "postgreserver" {
   name                   = var.postgresever_name
   resource_group_name    = azurerm_resource_group.postgres.name
@@ -10,5 +11,4 @@ resource "azurerm_postgresql_flexible_server" "postgreserver" {
   zone                   = "1"
   storage_mb = 32768
   sku_name   = "GP_Standard_D4s_v3"
-}
 }
