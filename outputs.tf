89e1e40e453078470a7e591cb1a04a792231c285
sureshkumarazdevops <108863097+sureshkumarazdevops@users.noreply.github.com>
2022-08-18 13:05:57 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,29 @@
+# Output Values - Resource Group
+output "resource_group_id" {
+  description = "Resource Group ID"
+  # Atrribute Reference
+  value = azurerm_resource_group.myrg.id 
+}
+output "resource_group_name" {
+  description = "Resource Group name"
+  # Argument Reference
+  value = azurerm_resource_group.myrg.name  
+}
+
+# Output Values - Postgres-Flexi server
+output "postgresql_flexible_administrator_login" {
+  description = "Administrator login for PostgreSQL Flexible server."
+  value       = azurerm_postgresql_flexible_server.postgresql_flexible_server.administrator_login
+  sensitive   = true
+}
+
+output "postgresql_flexible_administrator_password" {
+  description = "Administrator password for PostgreSQL Flexible server."
+  value       = var.administrator_password
+  sensitive   = true
+}
+
+output "postgresql_flexible_databases_names" {
+  description = "Map of databases names."
+  value       = azurerm_postgresql_flexible_server_database.postgresql_flexible_db
+}
